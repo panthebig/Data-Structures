c3cd0debe3d5af898e793ab118807c688b950dc4
pfilippos <63398108+pfilippos@users.noreply.github.com>
2020-04-12 19:35:09 +0300

    Add files via upload

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -12,40 +12,7 @@ BinaryTree::BinaryTree()
 
 void BinaryTree::CreateBinaryTree(std::string line)
 {
-    int i,m;
 
-    string wrd[1000];
-    string word="";
-
-    m=0;
-
-    for(i=0;i<=strlen(line.c_str());i++) //m shows the row and n the collum of raw m
-    {
-        if(line[i]!=' ')
-        {
-            word=word+line[i];
-        }
-        else
-        {
-            wrd[m]=word;
-            m++;
-            word="";
-        }
-    }
-    wrd[m]=word;
-
-   if(a.info.empty())
-        cout<<"NAII   ";
-
-
-
-
-    /*
-    cout<<"--"<<endl;
-    for(i=0;i<=m+1;i++)
-    {
-        cout<<wrd[i]<<endl;
-    }*/
 
 
 
--- main.cpp
+++ main.cpp
@@ -11,20 +11,70 @@ using namespace std;
 
 int main()
 {
+    string wrd[1000];
+    string word="";
+    int m,i;
+
     BinaryTree BiTree; //create new tree object in BinaryTree Class
 
 
     ifstream infile("inputfile.txt");   //opens the file named inputfile.txt
     std::string linestr;
 
+    m=0;
+
     while(getline(infile,linestr))
     {
         //use the remove_if to check if there are any punctuation marks in each lane and if there are any , remove them using .erase
         linestr.erase(remove_if(linestr.begin(), linestr.end(), ::ispunct), linestr.end());
-        //cout<<linestr<<endl;
 
-        BiTree.CreateBinaryTree(linestr);
+        /*for(i=0;i<=strlen(linestr.c_str());i++)   //allos tropos pou skeftika monos gia thn aferesh...
+        {
+            if(std::ispunct(linestr[i]))
+                linestr[i]=' ';
+        }*/
+
+
+
+        if(!(linestr.empty()))//if the line is not empty ,then and only then will the program try to saperate the words.
+        {
+
+
+            word="";//clears/resets string
+
+            for(i=0;i<=strlen(linestr.c_str());i++) //m shows the row
+            {
+                if(linestr[i]!='\t')//checking for tabs
+                {
+
+                    if(linestr[i]!=' ')
+                    {
+                        word=word+linestr[i];
+                    }
+                    else
+                    {
+                        wrd[m]=word;
+                        m++;
+                        word="";
+                    }
+
+                }
+            }
+            wrd[m]=word;
+            m++;
+
+        }
     }
+
+
+
+
+    for(i=0;i<=m;i++)
+    {
+        if((!wrd[i].empty()))//words with 0 length will not be eligible for any data stracture!
+            cout<<wrd[i]<<endl; //edo tha bei to BiTree.CreateBinaryTree(wrd[i]);
+    }
+
 
 
     return 0;
