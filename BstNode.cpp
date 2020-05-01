5c90b04872f9ef0d976e1624e2ac0c95c76e95bd
pfilippos <63398108+pfilippos@users.noreply.github.com>
2020-05-01 12:00:00 +0300

    Add files via upload

---
+++ BstNode.cpp
@@ -0,0 +1,156 @@
+#include <iostream>
+#include "BstNode.h"
+
+using namespace std;
+
+
+
+
+
+
+BstNode* GetNewNode(std::string idata)
+{
+    BstNode* newNode=new BstNode();
+    newNode->data=idata;
+    newNode->left=NULL;
+    newNode->right=NULL;
+    newNode->counter=1;
+    return newNode;
+}
+
+BstNode* Insert(BstNode* root,std::string data)
+{
+    if(root==NULL)
+    {
+        root=GetNewNode(data);
+        //cout<<data<<": -inserted"<<endl;
+        //return root;
+    }
+    else if(data < root->data)
+    {
+        root->left=Insert(root->left,data);
+        //return root;
+    }
+    else if(data > root->data)
+    {
+        root->right=Insert(root->right,data);
+        //return root;
+    }
+    else
+    {
+        //cout<<"einai mesa"<<endl;
+        root->counter++;
+    }
+    return root;
+
+}
+
+bool BstSearch(BstNode* root,std::string idata)
+{
+    if(root==NULL)
+    {
+        return false;
+    }
+    else if(root->data == idata)
+    {
+        cout<<root->counter<<endl;
+        return true;
+    }
+    else if(root->data > idata)
+    {
+        return BstSearch(root->left,idata);
+    }
+    else
+        return BstSearch(root->right,idata);
+}
+
+BstNode* DeleteEllement(BstNode* root,std::string idata,int i)
+{
+    struct BstNode* temp2;
+    if(i==0) //checks one time how many times idata is in the text
+    {
+        temp2=BstSearchNode(root,idata);
+        i++;
+    }
+
+    if(temp2->counter>=2)
+    {
+        cout<<"Before :  "<<temp2->counter<<endl;
+        temp2->counter--;
+        cout<<"after :  "<<temp2->counter<<endl;
+        return root;
+    }
+    else
+    {
+        if(root == NULL)
+        {
+            return root;
+        }
+        else if(root->data > idata)
+        {
+            root->left = DeleteEllement(root->left,idata,i);
+        }
+        else if(root->data < idata)
+        {
+            root->right = DeleteEllement(root->right,idata,i);
+        }
+        else
+        {
+            if( (root->left==NULL) && (root->right==NULL) ) //To node den exei kanena paidi
+            {
+                delete root;
+                root = NULL;
+            }
+            else if (root->left == NULL)//Den exei aristero paidi
+            {
+                struct BstNode *temp = root;//to temp deixnei ekei opou deixnei kai to root
+                root=root->right;
+                delete temp;
+            }
+            else if(root->right==NULL)//Den exei deksi paidi
+            {
+                struct BstNode *temp = root;//to temp deixnei ekei opou deixnei kai to root
+                root=root->left;
+                delete temp;
+            }
+            else//exei duo paidia
+            {
+                struct BstNode* temp = FindMin(root->right);
+                root->data=temp->data;//to node pou thelw na diagrapsw pairnei tin timi tou elaxistou node tou deksia ypodendrou
+                root->right= DeleteEllement(root->right,temp->data,i);
+            }
+
+        }
+        return root;
+    }
+
+}
+
+
+BstNode* FindMin(BstNode* root)
+{
+    while(root->left!=NULL)
+    {
+        root=root->left;
+    }
+    return root;
+}
+
+BstNode* BstSearchNode(BstNode* root,std::string idata)
+{
+    if(root==NULL)
+    {
+        return NULL;
+    }
+    else if(root->data == idata)
+    {
+        //cout<<root->counter<<endl;
+        return root;
+    }
+    else if(root->data > idata)
+    {
+        return BstSearchNode(root->left,idata);
+    }
+    else
+        return BstSearchNode(root->right,idata);
+}
--- main.cpp
+++ main.cpp
@@ -4,28 +4,74 @@
 #include <stdlib.h>
 #include <algorithm>
 #include <cstring>
+#include <vector>
 
-#include "BinaryTree.h"
+
+#include "BstNode.h"
+//#include "BinaryTree.h"
+//#include "Node.h"
 
 using namespace std;
 
 int main()
 {
-    BinaryTree BiTree; //create new tree object in BinaryTree Class
+    string *wrd=new string[8000000];
+    int m,i;
+    //int l;
 
 
-    ifstream infile("inputfile.txt");   //opens the file named inputfile.txt
+    //BinaryTree BiTree; //create new tree object in BinaryTree Class
+
+
+    ifstream infile("test.txt");   //opens the file named inputfile.txt
     std::string linestr;
+    vector<string> word;
 
-    while(getline(infile,linestr))
+    m=0;
+    while( infile >> linestr)
     {
-        //use the remove_if to check if there are any punctuation marks in each lane and if there are any , remove them using .erase
-        linestr.erase(remove_if(linestr.begin(), linestr.end(), ::ispunct), linestr.end());
-        //cout<<linestr<<endl;
-
-        BiTree.CreateBinaryTree(linestr);
+            linestr.erase(remove_if(linestr.begin(), linestr.end(), ::ispunct), linestr.end());
+            wrd[m]=linestr;
+            word.push_back(linestr);
+            m++;
     }
 
+    cout<<"Total words : "<<m<<endl<<endl;
+
+    BstNode *root=NULL;
+    //l=0;
+
+    cout<<"Creating Binary Search Tree..."<<endl;
+    for(i=0;i<m;i++)
+    {
+        root = Insert(root,wrd[i]);
+
+    }
+    cout<<"Binary Search Tree has been created!"<<endl;
+
+
+    std::string searchword;
+    cin>>searchword;
+
+    if(BstSearch(root,searchword)==true)
+        cout<<"The word : "<<searchword<<" has been successfully Found!"<<endl;
+    else
+        cout<<"The word : "<<searchword<<" has NOT been Found!"<<endl;
+
+
+    std::string searchword1;
+    cin>>searchword1;
+
+    DeleteEllement(root,searchword1,0);
+    cout<<"The word : "<<searchword1<<" has been DELETED"<<endl;
+
+    std::string searchword3;
+    cin>>searchword3;
+
+    if(BstSearch(root,searchword3)==true)
+        cout<<"The word : "<<searchword3<<" has been successfully Found!"<<endl;
+    else
+        cout<<"The word : "<<searchword3<<" has NOT been Found!"<<endl;
 
     return 0;
 }
