47833070026a12382e65f5aba066886006c7ee79
pfilippos <63398108+pfilippos@users.noreply.github.com>
2020-05-16 16:54:33 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -5,27 +5,122 @@
 #include <algorithm>
 #include <cstring>
 
-#include "BinaryTree.h"
+
+
+#include "BstNode.h"
+#include "AvlTree.h"
 
 using namespace std;
 
 int main()
 {
-    BinaryTree BiTree; //create new tree object in BinaryTree Class
+    string *wrd=new string[8000000];
+    int m,i;
 
 
-    ifstream infile("inputfile.txt");   //opens the file named inputfile.txt
+    ifstream infile("test.txt");   //opens the file named inputfile.txt
     std::string linestr;
 
-    while(getline(infile,linestr))
-    {
-        //use the remove_if to check if there are any punctuation marks in each lane and if there are any , remove them using .erase
-        linestr.erase(remove_if(linestr.begin(), linestr.end(), ::ispunct), linestr.end());
-        //cout<<linestr<<endl;
 
-        BiTree.CreateBinaryTree(linestr);
+    m=0;
+    while( infile >> linestr)
+    {
+            linestr.erase(remove_if(linestr.begin(), linestr.end(), ::ispunct), linestr.end());
+            wrd[m]=linestr;
+            m++;
+
     }
 
+    cout<<"Total words : "<<m<<endl<<endl;
+
+    //BS Tree
+
+    BstNode *BSTroot=NULL;
+    //l=0;
+
+    cout<<"Creating Binary Search Tree..."<<endl;
+    for(i=0;i<m;i++)
+    {
+        BSTroot = Insert(BSTroot,wrd[i]);
+
+    }
+    cout<<"Binary Search Tree has been created!"<<endl;
+/*
+    PreOrder(BSTroot);
+    cout<<endl;
+    InOrder(BSTroot);
+    cout<<endl;
+    PostOrder(BSTroot);
+    cout<<endl;
+*/
+
+    std::string searchword;
+    cin>>searchword;
+
+    if(BstSearch(BSTroot,searchword)==true)
+        cout<<"The word : "<<searchword<<" has been successfully Found!"<<endl;
+    else
+        cout<<"The word : "<<searchword<<" has NOT been Found!"<<endl;
+
+
+    std::string searchword1;
+    cin>>searchword1;
+
+    DeleteEllement(BSTroot,searchword1,0);
+    cout<<"The word : "<<searchword1<<" has been DELETED"<<endl;
+
+    std::string searchword3;
+    cin>>searchword3;
+
+    if(BstSearch(BSTroot,searchword3)==true)
+        cout<<"The word : "<<searchword3<<" has been successfully Found!"<<endl;
+    else
+        cout<<"The word : "<<searchword3<<" has NOT been Found!"<<endl;
+
+
+
+    //AVL TREE
+
+    AvlTreeNode *AVLroot=NULL;
+
+    cout<<"Creating Avl Tree..."<<endl;
+    for(i=0;i<m;i++)
+    {
+        AVLroot = AvlInsertion(AVLroot,wrd[i]);
+
+    }
+    cout<<"AVL Tree has been created!"<<endl;
+/*
+    preorder(AVLroot); //Gia test kai epalitheush oti to avl insertion leitourgei mono!
+    cout<<endl;
+    postorder(AVLroot);
+    cout<<endl;
+    inorder(AVLroot);
+    cout<<endl;
+*/
+    std::string searchword4;
+    cin>>searchword4;
+
+    if(AvlSearch(AVLroot,searchword4)==true)
+        cout<<"The word : "<<searchword4<<" has been successfully Found!"<<endl;
+    else
+        cout<<"The word : "<<searchword4<<" has NOT been Found!"<<endl;
+
+    std::string searchword5;
+    cin>>searchword5;
+
+    AVLdelete(AVLroot,searchword5,0);
+    cout<<"The word : "<<searchword5<<" has been DELETED"<<endl;
+
+
+
+    std::string searchword6;
+    cin>>searchword6;
+
+    if(AvlSearch(AVLroot,searchword6)==true)
+        cout<<"The word : "<<searchword6<<" has been successfully Found!"<<endl;
+    else
+        cout<<"The word : "<<searchword6<<" has NOT been Found!"<<endl;
 
     return 0;
 }
